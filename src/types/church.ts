24f0874e54ea2e5e54099296d5a14deee59d17cc
Gabriel Sousa <gaabdev42@gmail.com>
2026-09-02 15:10:38 -0300

    feat: add church type and mock data

---
+++ src/types/church.ts
@@ -0,0 +1,11 @@
+export interface Church {
+    id: number;
+    name: string;
+    address: string;
+    neighborhood: string;
+    lat: number;
+    lng: number;
+    isOpen: boolean;
+    massTime : string[];
+    confessionTimes?: string[];
+}
